2926cf3e78f76e3b216374517eda707e96356a80
fbara <fbara1@yahoo.com>
2025-04-19 15:24:04 -0400

    Initial Commit

---
+++ AIPromptGen/AIPromptGenApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIPromptGenApp.swift
+//  AIPromptGen
+//
+//  Created by Frank Bara on 4/19/25.
+//
+
+import SwiftUI
+
+@main
+struct AIPromptGenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
